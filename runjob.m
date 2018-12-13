a3b1159ece9e9a062a999dbb660740bb61a03757
Srivas Chennu <srivas@gmail.com>
2018-12-13 00:00:29 +0000

    Checked in Thu Dec 13 00:00:29 GMT 2018

--- runjob.m
+++ runjob.m
@@ -17,7 +17,7 @@ tasklist = {
 % 
 %     'rereference' '{subjlist{subjidx,1} 1 1 ''''}'
 %     'checktrials' '{subjlist{subjidx,1} 60 ''''}'
-    'calcftspec' 'subjlist(subjidx,1)'
+    'fitdcm' 'subjlist(subjidx,1)'
 %     'plotftspec' 'subjlist(subjidx,1)'
 %     'ftcoherence' 'subjlist(subjidx,1)'
 %     'calcgraph' '{subjlist{subjidx,1}}'
